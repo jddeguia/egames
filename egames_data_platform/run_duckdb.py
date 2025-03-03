5b8488b36b73ae371f50b10a3774ef3869510464
jddeguia <justin.dayaodeguia@gmail.com>
2025-03-03 16:04:30 +0800

    export csv files from duckdb

--- egames_data_platform/run_duckdb.py
+++ egames_data_platform/run_duckdb.py
@@ -1,41 +1,25 @@
-# import duckdb
-
-# # Connect to the DuckDB database
-# conn = duckdb.connect(database='./dev.duckdb', read_only=False)
-
-# # List of tables to query
-# tables = [
-#     "mart_daily_order_kpi_metrics",
-#     "mart_fct_order_transactions",
-#     "dim_customers",
-#     "dim_products"
-# ]
-
-# # Loop through each table and export its data to a CSV file
-# for table in tables:
-#     query = f"SELECT * FROM {table}"
-#     output_file = f"{table}.csv"
-#     # Execute the query and save the result to a CSV file
-#     conn.execute(query).df().to_csv(output_file, index=False)
-#     print(f"Data exported to {output_file}")
-
-# # Close the connection
-# conn.close()
-
 import duckdb
 
 # Connect to the DuckDB database
-conn = duckdb.connect(database='./dev.duckdb', read_only=True)
+conn = duckdb.connect(database='./dev.duckdb', read_only=False)
 
+# List of tables to query
+tables = [
+    "mom_analysis_by_domain",
+    "mom_analysis_by_region",
+    "mom_analysis_by_sport",
+    "yoy_analysis_by_domain",
+    "yoy_analysis_by_region",
+    "yoy_analysis_by_sport",
+]
 
-# Query to select the first 5 rows
-query = f"SELECT * FROM mom_analysis_by_region ORDER BY year, month_number DESC"
-
-# Execute the query and fetch the results
-df = conn.execute(query).fetchdf()
-
-# Print the result
-print(df)
+# Loop through each table and export its data to a CSV file
+for table in tables:
+    query = f"SELECT * FROM {table}"
+    output_file = f"{table}.csv"
+    # Execute the query and save the result to a CSV file
+    conn.execute(query).df().to_csv(output_file, index=False)
+    print(f"Data exported to {output_file}")
 
 # Close the connection
 conn.close()
